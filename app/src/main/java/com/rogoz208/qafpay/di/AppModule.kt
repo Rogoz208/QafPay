1d28748302f66c77bf0d4b1b6b4673c9d0c700ee
Rogoz208 <rogoz208@gmail.com>
2024-08-20 21:49:42 +0300

    Added repository implementation, DI and use-cases

---
+++ app/src/main/java/com/rogoz208/qafpay/di/AppModule.kt
@@ -0,0 +1,61 @@
+package com.rogoz208.qafpay.di
+
+import com.rogoz208.qafpay.BuildConfig
+import com.rogoz208.qafpay.data.QafPayApi
+import com.rogoz208.qafpay.data.repos.QafPayRepositoryImpl
+import com.rogoz208.qafpay.domain.repos.QafPayRepository
+import com.rogoz208.qafpay.domain.use_cases.AuthUseCases
+import com.rogoz208.qafpay.domain.use_cases.OtpSendUseCase
+import com.rogoz208.qafpay.domain.use_cases.OtpVerifyUseCase
+import com.rogoz208.qafpay.domain.use_cases.SessionCloseUseCase
+import com.rogoz208.qafpay.domain.use_cases.SessionOpenUseCase
+import com.rogoz208.qafpay.domain.use_cases.SessionTestUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient? = if (BuildConfig.DEBUG) {
+        val logging = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    } else {
+        null
+    }
+
+    @Provides
+    @Singleton
+    fun provideQafPayApi(okHttpClient: OkHttpClient?): QafPayApi = QafPayApi(
+        baseUrl = BuildConfig.QAF_PAY_API_BASE_URL,
+        okHttpClient = okHttpClient
+    )
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(api: QafPayApi): QafPayRepository {
+        return QafPayRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthUseCases(repo: QafPayRepository): AuthUseCases {
+        return AuthUseCases(
+            SessionTestUseCase(repo),
+            SessionOpenUseCase(repo),
+            SessionCloseUseCase(repo),
+            OtpSendUseCase(repo),
+            OtpVerifyUseCase(repo)
+        )
+    }
+}
